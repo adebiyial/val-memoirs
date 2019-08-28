9399dc80bcb0ac304e0ff8048e364397a75c8c5b
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-08-28 18:16:42 +0100

    Refactor component to use dummy data

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import '../styles/stories.scss';
 
-// image - dummy
-import imgBlob from '../blob.png';
+// Dummy data
+import STORIES from '../utils/_dummydata';
+import { uuid, isCharacterLimitExceeded, truncateCharacter } from '../utils/fx';
 
 function Stories() {
-  const arr = [1, 2, 3, 4, 5];
+  const [stories] = React.useState(STORIES);
+
+
   return (
     <div className="stories">
       <div className="container">
@@ -16,38 +19,56 @@ function Stories() {
           </header>
           <section className="storiesGallery">
             <ul className="listOfStories">
-              {arr.map((nonsense, index) => (
-                <li className="story" key={index}>
-                  <div className="storyContent">
-                    <h3 className="storyTitle">
-                      Summitech Frontend Developer Test
-                    </h3>
-                    <div>
-                      <img src={imgBlob} alt="blob" className="storyImg" />
-                      <div className="storyInfoBlock">
-                        <header className="storyHeader">
-                          <span>adebiyial</span>
-                          <div className="storyCtaBlock">
-                            <div className="likeButtonBlock">
-                              <button type="submit" className="likeButton">
-                                +
-                              </button>
+              {stories.map(
+                ({ headline, image, username, noOfLikes, description }) => (
+                  <li className="story" key={uuid()}>
+                    <div className="storyContent">
+                      <h3 className="storyHeadline">{headline}</h3>
+                      <div>
+                        <div className="storyImgBlock">
+                          <img
+                            src={image}
+                            alt={headline}
+                            className="storyImg"
+                          />
+                        </div>
+                        <div className="storyInfoBlock">
+                          <header className="storyHeader">
+                            <span className="username">{username}</span>
+                            <div className="storyCtaBlock">
+                              <div className="likeButtonBlock">
+                                <button type="submit" className="likeButton">
+                                  <svg viewBox="0 0 24 24" className="likeIcon">
+                                    <g>
+                                      <path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
+                                    </g>
+                                  </svg>
+                                </button>
+                                <span className="noOfLikes">{noOfLikes}</span>
+                              </div>
                             </div>
-                            <span className="noOfLikes">300</span>
+                          </header>
+                          <div className="storyDescription">
+                            <span>
+                              {(isCharacterLimitExceeded(description) &&
+                                truncateCharacter(description)) ||
+                                description}
+                            </span>
+                            {isCharacterLimitExceeded(description) && (
+                              <>
+                                <span>...</span>
+                                <button className="readMoreBtn">
+                                  read more
+                                </button>
+                              </>
+                            )}
                           </div>
-                        </header>
-                        <div className="story">
-                          <p>
-                            A friendly community, books, webinars, conferences
-                            and fancy cats. Starting from $3 a month (or yearly
-                            payments). You can cancel any time.
-                          </p>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              ))}
+                  </li>
+                ),
+              )}
             </ul>
           </section>
         </div>
