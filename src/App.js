09c2732a4127dc35a8eeabff6231b78eaf2393f7
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-08-28 09:36:22 +0100

    Add imports for components and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// Components
+import GlobalNav from './components/GlobalNav';
+import GlobalHeading from './components/GlobalHeading';
+import Stories from './components/Stories';
+
+// Styles
+import './styles/normalize.scss';
+import './styles/index.scss';
+import './styles/common.scss';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <GlobalNav />
+      <GlobalHeading />
+      <Stories />
     </div>
   );
 }
