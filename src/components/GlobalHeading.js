66bf33ea38f1724d625fe4eaa2d96edcaddb11d0
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-08-28 10:06:34 +0100

    Add import and component content
    Add component markup for global heading, and styling import

---
+++ src/components/GlobalHeading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Styles
+import '../styles/globalheading.scss';
+
+function GlobalHeading() {
+  return (
+    <div className="globalHeading">
+      <div className="container">
+        <div className="globalHeadingContent">
+          <h1 className="headline">Awesome Valentine Memories</h1>
+          <p className="headlineInfo">
+            Create, share, and view awesome memories from last Valentine.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GlobalHeading;
