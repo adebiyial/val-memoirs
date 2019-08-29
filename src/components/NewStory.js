604c15b86daab9c02b38fad1389f5f2709c4c80a
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-08-29 10:35:06 +0100

    Add component for new story

---
+++ src/components/NewStory.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import '../styles/newstory.scss';
+import NewStoryForm from './NewStoryForm';
+
+function NewStory() {
+  const [isModalActive, setIsModalActive] = React.useState(true);
+
+  function showModal() {
+    setIsModalActive(true);
+    document.body.classList.add('fixed-body');
+  }
+
+  function hideModal() {
+    setIsModalActive(false);
+    document.body.classList.remove('fixed-body');
+  }
+
+  return (
+    <div className="newStoryBlock">
+      <NewStoryForm
+        isModalActive={isModalActive}
+        showModal={showModal}
+        hideModal={hideModal}
+      />
+      <div className="newStoryCTABlock">
+        <button className="newStoryBtn floatingBtn" onClick={showModal}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NewStory;
