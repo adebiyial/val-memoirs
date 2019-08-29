9165bb4ccdf99429fa7e3819e0a727ad13de1dbd
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-08-29 07:10:19 +0100

    Switch daya type on noOfLikes key

--- src/utils/_dummydata.js
+++ src/utils/_dummydata.js
@@ -3,7 +3,7 @@ const STORIES = [
     headline: 'from russia with love',
     image: '/assets/images/blob.png',
     username: 'jamesbond',
-    noOfLikes: '400',
+    noOfLikes: 400,
     description: `I hope you feel loved and appreciated on Valentine’s Day. Because you are the love I've been dreaming of,
       thank you for coming into my life and making me happy. You're the best part of my everyday.`,
   },
@@ -11,7 +11,7 @@ const STORIES = [
     headline: 'revolutionary road',
     image: '/assets/images/blob2.png',
     username: 'leodicarprio',
-    noOfLikes: '4K',
+    noOfLikes: 4000,
     description: `Wish we could be together swapping chocolates and laughing over candy hearts like we used to.
         We've grown now, and farther away from each other. The solitary nights have turned to day, right before
         our eyes. Oh my darling, stay in this darkness with me. With you I feel safe.
@@ -21,21 +21,21 @@ const STORIES = [
     headline: "sometimes it's something, sometimes it's nothing at all",
     image: '/assets/images/blob3.png',
     username: 'passenger',
-    noOfLikes: '100',
+    noOfLikes: 100,
     description: `Here's to a Valentine’s Day filled with good wine, good food and especially good friends like you.`,
   },
   {
     headline: 'random access memories',
     image: '/assets/images/blob4.png',
     username: 'daftPunk',
-    noOfLikes: '200',
+    noOfLikes: 200,
     description: `Hope your day brings just what you want it to.`,
   },
   {
     headline: 'viva la vida',
     image: '/assets/images/blob5.png',
     username: 'coldplay',
-    noOfLikes: '350',
+    noOfLikes: 350,
     description: `Happy Valentine’s Day, Sweetheart.`,
   },
 ];
